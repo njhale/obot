ba02fa5a6fcfef2f7b4f6b87ecc27be1c5a13e81
Nick Hale <4175918+njhale@users.noreply.github.com>
2024-12-13 17:26:41 -0500

    wip
    Signed-off-by: Nick Hale <4175918+njhale@users.noreply.github.com>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,6 +1,8 @@
 package version
 
 import (
+	"runtime/debug"
+
 	"github.com/otto8-ai/nah/pkg/version"
 )
 
@@ -9,5 +11,10 @@ var (
 )
 
 func Get() version.Version {
-	return version.NewVersion(Tag)
+	// return version.NewVersion(Tag)
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return version.NewVersion(Tag)
+	}
+	return version.NewVersion(bi.Main.Version)
 }
